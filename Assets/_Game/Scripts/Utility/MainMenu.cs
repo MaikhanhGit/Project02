da38cb89d27fc5b875bcf159aa932ae098617feb
Maikhanh <maikhanh.dsn@gmail.com>
2023-03-10 00:00:20 -0600

    MusicPlayer

---
+++ Assets/_Game/Scripts/Utility/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   
+    [SerializeField] AudioClip _music;
+    [SerializeField] AudioClip _ambience;
+    [SerializeField] AudioClip _onButtonClick;    
+    [SerializeField] float _musicVolume = .3f;
+    [SerializeField] float _ambienceVolume = .5f;
+
+    private void Start()
+    {
+        MusicPlayer.Instance.PlayNewSong(_music, _ambience, _musicVolume, _ambienceVolume);
+    }
+
+    public void LoadScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void OnButtonClickSFX()
+    {
+        if (_onButtonClick)
+            AudioHelper.PlayClip2D(_onButtonClick, 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
---
+++ Assets/_Game/Scripts/Utility/MusicPlayer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+
+public class MusicPlayer : SingletonMB<MusicPlayer>
+{
+    [SerializeField] Slider _musicSlider;
+    AudioSource _music;
+    AudioSource _ambience;
+    float _musicVolume = .3f;
+    float _ambienceVolume = .5f;
+
+    private void Awake()
+    {
+        _music = gameObject.AddComponent<AudioSource>();
+        _music.loop = true;
+        _ambience = gameObject.AddComponent<AudioSource>();
+        _ambience.loop = true;
+    }
+
+    // this music player is specialized to play 2 audio clips at once
+    public void PlayNewSong(AudioClip newSong01, AudioClip newSong02, float volume01, float volume02)
+    {
+        if (newSong01 == null && newSong02 == null) return;    // guard clause
+
+        if (newSong01 != null)
+        {
+            _music.clip = newSong01;
+            _music.volume = volume01;
+            _music.Play();
+        }
+        if (newSong02 != null)
+        {
+            _ambience.clip = newSong02;
+            _ambience.volume = volume02;
+            _ambience.Play();
+        }
+    }
+
+    private void Update()
+    {
+        _music.volume = _musicSlider.value;
+        _ambience.volume = _ambienceVolume;
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        _musicVolume = volume;
+    }
+
+    public void UpdateAmbienceVolume(float volume)
+    {
+        _ambienceVolume = volume;
+    }
+}
